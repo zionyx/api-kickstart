55288294ef721f515bc44fdfa8dba3f1b4fed306
Kirsten Jones <synedra@gmail.com>
2015-05-21 16:54:10 -0600

    Adding license information to scripts in api-kickstart repo

--- examples/python/network-lists.py
+++ examples/python/network-lists.py
@@ -1,5 +1,20 @@
 #! /usr/bin/python
-""" Sample client for network-lists
+""" Copyright 2015 Akamai Technologies, Inc. All Rights Reserved.
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+
+ You may obtain a copy of the License at 
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+Sample client for network-lists
 In order to "create" a new list, you'll want to 
 remove the # at the beginning of the "createNetworkList" call
 and update the IP addresses to be appropriate for your needs.
--- examples/python/user-admin.py
+++ examples/python/user-admin.py
@@ -1,10 +1,24 @@
 #! /usr/bin/python
-"""
+""" Copyright 2015 Akamai Technologies, Inc. All Rights Reserved.
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+
+ You may obtain a copy of the License at 
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+
 A simple script demonstrating how to add new users and delete them.
 Note that this isn't a great script to run on your production system,
 but it will create a user unlikely to conflict with the ones you have.
 This is more of sample code than anything else.
-
 
 """
 
