9408bd50e45e7b1923a52c805fe518ef3bc63789
Hunter, Kirsten <khunter@akamai.com>
2015-07-18 18:06:05 +1000

    Work on logging in user-admin

--- examples/python/http_calls.py
+++ examples/python/http_calls.py
@@ -115,11 +115,13 @@ class EdgeGridHttpCaller():
                   path = endpoint
           endpoint_result = self.session.put(urljoin(self.baseurl,path), data=body, headers=headers)
           if self.verbose: print ">>>\n" + json.dumps(endpoint_result.json(), indent=2) + "\n<<<\n"
+          status = endpoint_result.status_code
 	  if self.verbose: print "LOG: PUT %s %s %s" % (endpoint,status,endpoint_result.headers["content-type"])
           return endpoint_result.json()
 
     def deleteResult(self, endpoint):
           endpoint_result = self.session.delete(urljoin(self.baseurl,endpoint))
           if self.verbose: print ">>>\n" + json.dumps(endpoint_result.json(), indent=2) + "\n<<<\n"
+	  status = endpoint_result.status_code
 	  if self.verbose: print "LOG: DELETE %s %s %s" % (endpoint,status,endpoint_result.headers["content-type"])
           return endpoint_result.json()
